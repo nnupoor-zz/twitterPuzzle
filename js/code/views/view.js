c94485f78b2c3a07d31c79a622900410935e382a
nnupoor <nupoor.neha@gmail.com>
2014-01-30 12:45:36 +0530

    test cases covered

--- js/code/views/view.js
+++ js/code/views/view.js
@@ -45,7 +45,8 @@ var myApp = Backbone.View.extend({
 		if(data.length===11){
 				var links = [{"source": 0,"target": 0},{"source": 1,"target": 0},{"source": 2, "target": 0},{"source": 3,"target": 0},{"source": 4,"target": 0},{"source": 5,"target": 0},{"source": 6,"target": 0},{"source": 7,"target": 0},{"source": 8,"target": 0},{"source": 9,"target": 0},{"source": 10,"target": 0}];	
 		}else{
-				var links = [{"source": 0,"target": 0},{"source": 1,"target": 0},{"source": 2, "target": 0},{"source": 3,"target": 0},{"source": 4,"target": 0},{"source": 5,"target": 0},{"source": 6,"target": 0},{"source": 7,"target": 0},{"source": 8,"target": 0},{"source": 9,"target": 0}];				
+				var l = [{"source": 0,"target": 0},{"source": 1,"target": 0},{"source": 2, "target": 0},{"source": 3,"target": 0},{"source": 4,"target": 0},{"source": 5,"target": 0},{"source": 6,"target": 0},{"source": 7,"target": 0},{"source": 8,"target": 0},{"source": 9,"target": 0}];				
+				var links = _.first(l,data.length);
 		}
 		var json = {links:links,nodes:data};
 		renderFDGraph(json);
